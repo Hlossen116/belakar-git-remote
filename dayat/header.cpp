a26cada38c2aa1ef8883934327ac56f57ecd42a1
HLossen <136354950+Hlossen116@users.noreply.github.com>
2023-11-20 21:17:40 +0700

    Update header.cpp
    baru lagi buat masalah

--- dayat/header.cpp
+++ dayat/header.cpp
@@ -3,4 +3,15 @@
 using namespace std;
 int main(){
       int b = 0;
+
+      #include <iostream>
+using namespace std;
+int main(){
+      for(int a = 1;a <=10;a++){
+            for(int b = 1;b<=a;b++){
+                  cout<<a;
+            }
+            cout<<endl;
+      }
+}
 }
